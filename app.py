df5b9b2a4c017359c87587c64018218f3853c946
Rajesh3655 <138114625+Rajesh3655@users.noreply.github.com>
2025-06-16 00:06:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -821,8 +821,8 @@ def update_election_status_mm(election_id):
 # ========= SERVER =========
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
