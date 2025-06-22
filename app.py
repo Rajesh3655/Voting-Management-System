3c0a84a856ffcddb244ae2f65fa9a3e85bd7e7ee
Rajesh3655 <138114625+Rajesh3655@users.noreply.github.com>
2025-06-22 23:06:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -803,7 +803,7 @@ def serve_static(path):
 # ========= SERVER =========
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
 
 # if __name__ == '__main__':
