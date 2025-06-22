fd33d7093ada234599cb4a256fc917c05b42cb41
Rajesh3655 <138114625+Rajesh3655@users.noreply.github.com>
2025-06-22 22:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from pymongo import ASCENDING
-
+from werkzeug.security import generate_password_hash
 # Init Flask app
 app = Flask(__name__, static_folder="frontend", static_url_path="/")
 CORS(app)
